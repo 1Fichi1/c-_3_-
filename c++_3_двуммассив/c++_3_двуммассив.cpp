44a074e35509f264cc6ec95e8f4a44f914a5d98d
1Fichi1 <anderpen2006time@gmail.com>
2024-02-19 22:22:53 +0300

    C3двуммасив

---
+++ c++_3_двуммассив/c++_3_двуммассив.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <iomanip>
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    const int rows = 3;
+    const int cols = 5;
+    double array[rows][cols];
+
+    std::cout << "Введите элементы массива построчно, разделяя числа пробелом:" << std::endl;
+
+    for (int i = 0; i < rows; ++i) {
+        std::cout << "Строка " << i + 1 << ": ";
+        for (int j = 0; j < cols; ++j) {
+            std::cin >> array[i][j];
+        }
+    }
+    for (int i = 0; i < rows; ++i) {
+        double sum = 0;
+        for (int j = 0; j < cols; ++j) {
+            sum += array[i][j];
+        }
+        double average = sum / cols;
+        std::cout << "Среднее арифметическое строки " << i + 1 << ": " << std::fixed << std::setprecision(2) << average << std::endl;
+    }
+
+    return 0;
+}
